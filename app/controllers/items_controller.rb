3f3a8648bb528b52e6023deccca30a4506d6484c
Jo Kakomba <ariel.kakomba@gmail.com>
2015-12-23 04:11:55 +0000

    buat items controller, buat tampilan ke root items jadi index, buat tampilan tambah checklist item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,27 @@
 class ItemsController < ApplicationController
+  
+  
+    def index
+      @items = Item.all.order("created_at DESC")
+    end
+    
+    def new
+      @item = Item.new
+    end
+    
+    def create 
+      @item = Item.new(params.require(:item).permit(:title, :description))
+      if @item.save
+        redirect_to root_path
+      else 
+        render 'new'
+      end
+    end
+    
+    private
+    
+      def item_params
+        params.require(:item).permit(:title, :description)
+      end
+    
 end
