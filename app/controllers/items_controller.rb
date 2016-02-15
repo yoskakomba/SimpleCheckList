015cde4c55f3f198687df048e5039c68751144bc
Jo Kakomba <ariel.kakomba@gmail.com>
2016-02-15 04:24:15 +0000

    updating items controller and add user

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,12 @@
 class ItemsController < ApplicationController
-  
+    before_action :find_item, only: [:show, :edit, :update, :destroy]
   
     def index
       @items = Item.all.order("created_at DESC")
+    end
+    
+    def show
+      @item = Item.find(params[:id])
     end
     
     def new
@@ -10,7 +14,7 @@ class ItemsController < ApplicationController
     end
     
     def create 
-      @item = Item.new(params.require(:item).permit(:title, :description))
+      @item = Item.new(item_params)
       if @item.save
         redirect_to root_path
       else 
@@ -18,10 +22,31 @@ class ItemsController < ApplicationController
       end
     end
     
+    def edit
+      
+    end
+    
+    def update
+      if @item.update(item_params)
+        redirect_to item_path(@item)
+      else
+        render 'edit'
+      end
+    end
+    
+    def destroy
+      @item.destroy
+      redirect_to root_path
+    end
+    
     private
     
       def item_params
         params.require(:item).permit(:title, :description)
+      end
+      
+      def find_item
+       @item = Item.find(params[:id])
       end
     
 end
